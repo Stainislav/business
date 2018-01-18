562279f2ec3afc0665a3d6f30d9624e96a1a2a9a
Your Name <swatogor@yandex.ru>
2018-01-18 22:02:33 +0300

    I have added Russian comments from my Spider task.

--- business/models.py
+++ business/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 
+# Район города имеет название и ID (ID автоматическое).
 class City_region(models.Model):
+    # Название. 
     name = models.CharField(max_length=200)
     
     def __str__(self):
         return self.name
 
-
+# Категория имеет название и ID (ID автоматическое).
 class Category(models.Model):
+    # Название.
     name = models.CharField(max_length=200)
     
     def __str__(self):
@@ -21,24 +24,43 @@ class Enterprise_network(models.Model):
     def __str__(self):
         return self.name
 
-
+# Предприятие имеет название, описание и ID (ID автоматическое).
 class Enterprise(models.Model):
+    # Название.
     name = models.CharField(max_length=200)
+
+    # Описание.
     description = models.CharField(max_length=200)
+
+    # Принадлежит одной из сети предприятий. 
     enterprise_network = models.ForeignKey(Enterprise_network, on_delete=models.CASCADE)    
+
+    # Имеет принадлежность к нескольким районам города, может быть представлена сразу в нескольких.
     city_region = models.ManyToManyField(City_region)  
+    
+    # Имеет список предоставляемых услуг\товаров с ценами.
     services_list = []
 
     def __str__(self):
         return self.name
 
-
+# Услуга\товар имеет название, категорию и ID (ID автоматическое).
 class Service(models.Model):
+    # Название.
     name = models.CharField(max_length=200)
-    price = models.CharField(max_length=200)
+    
+    # Категория.
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    # Может продаваться в любом предприятии в сети.
     enterprise = models.ManyToManyField(Enterprise)
 
+    # Цена может отличаться в зависимости от предприятия.
+    price = models.CharField(max_length=200)
+    
     def __str__(self):
         return self.name
+
+            
+            
 
