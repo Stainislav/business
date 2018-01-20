44dc47eb09c693356177adc696877f2265e37ae2
Your Name <swatogor@yandex.ru>
2018-01-20 21:30:23 +0300

    I renamed the business.Enterprise model to Organisation.

--- business/views.py
+++ business/views.py
@@ -1,9 +1,9 @@
-from .models import Enterprise, Service
+from .models import Organisation, Service
 from django.shortcuts import render
 
 
 def index(request):
-    enterprise_list = Enterprise.objects.order_by('-city_region')[:5]
+    enterprise_list = Organisation.objects.order_by('-city_region')[:5]
     context = {'enterprise_list': enterprise_list}
     return render(request, 'business/index.html', context)
 
