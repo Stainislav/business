0b235e0d84e79bbfb10174964f0590f4c79b0ffa
Stanislav <swatogor@mail.ru>
2018-03-08 17:53:05 +0200

    I've added searching and filtering to the enterprise_network model on an admin page.

--- business/admin.py
+++ business/admin.py
@@ -27,9 +27,18 @@ class CategoryAdmin(admin.ModelAdmin):
     class Meta:
         model = Category
 
+
+class Enterprise_networkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+
+    class Meta:
+        model = Enterprise_network
+
 admin.site.register(District, DistrictAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Enterprise_network)
+admin.site.register(Enterprise_network, Enterprise_networkAdmin)
 admin.site.register(Organisation, OrganisationAdmin)
 admin.site.register(Service)
 admin.site.register(Membership)
--- business/admin.py~
+++ business/admin.py~
@@ -18,8 +18,17 @@ class DistrictAdmin(admin.ModelAdmin):
     class Meta:
         model = District
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+
+    class Meta:
+        model = Category
+
 admin.site.register(District, DistrictAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Enterprise_network)
 admin.site.register(Organisation, OrganisationAdmin)
 admin.site.register(Service)
