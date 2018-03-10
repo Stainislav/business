8d894d9c9a1447cc48d5e9e71163d0e535141030
Stanislav <swatogor@mail.ru>
2018-03-10 19:20:57 +0200

    I'ved added the 'OrganisationDetailAPIView' path to business.api.urls.

--- business/api/urls.py
+++ business/api/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import (OrganisationListAPIView)
+from .views import (
+    OrganisationListAPIView, 
+    OrganisationDetailAPIView
+    )
 
 
 urlpatterns = [
     url(r'^$', OrganisationListAPIView.as_view(), name='list'),
+    url(r'^(?P<district_id>\d+)/$', OrganisationDetailAPIView.as_view(), name='detail'),    
 ]  
 '''
 from django.conf.urls import url
