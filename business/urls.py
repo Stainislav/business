61b68584c8d50cf8f2a98844c96936a85f186921
Stanislav <swatogor@mail.ru>
2018-03-10 21:03:22 +0200

    I've deleted redundant urls.

--- business/urls.py
+++ business/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^organizations/$', views.organization_list),
-    url(r'^organizations/(?P<pk>[0-9]+)/$', views.organization_by_district),    
+    url(r'^$', views.index, name='index')
 ]
