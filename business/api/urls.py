7aaf56ea5bdecb67f828328ee596dcc17db510d4
Stanislav <swatogor@mail.ru>
2018-03-25 13:04:12 +0200

    I've added urls for Organisation and Service detail views. And a url for filtering organisations by district.

--- business/api/urls.py
+++ business/api/urls.py
@@ -3,12 +3,16 @@ from django.contrib import admin
 
 from .views import (
     OrganisationListAPIView, 
-    OrganisationDetailAPIView
+    OrganisationDetailAPIView,
+    ServiceDetailAPIView,
+    OrganisationByDistrictAPIView
     )
 
 
 urlpatterns = [
     url(r'^$', OrganisationListAPIView.as_view(), name='list'),
-    url(r'^(?P<district_id>\d+)/$', OrganisationDetailAPIView.as_view(), name='detail'),    
-]  
+    url(r'^(?P<district_id>\d+)/$', OrganisationByDistrictAPIView.as_view(), name='district'),    
+    url(r'^service/(?P<service_id>\d+)/$', ServiceDetailAPIView.as_view(), name='service'),
+    url(r'^details/(?P<organisation_id>\d+)/$', OrganisationDetailAPIView.as_view(), name='organisation'),    
+]
 
