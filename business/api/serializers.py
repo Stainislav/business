009ea83fab5f7a0f56632153d0cc1ae2ab40582d
Stanislav <swatogor@mail.ru>
2018-03-25 12:59:32 +0200

    I've created Service Serializer.

--- business/api/serializers.py
+++ business/api/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import StringRelatedField
 
-from business.models import Organisation
+from business.models import Organisation, Service
 
 class OrganisationSerializer(ModelSerializer):
     goods = StringRelatedField(many=True)
-    
+    district = StringRelatedField(many=True)
+
     class Meta:
         model = Organisation
         fields = ('id', 'name', 'description', 'organization_network', 'district', 'goods')
 
+
+class ServiceSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Service
+        fields = ('id', 'name', 'category')
+ 
 
