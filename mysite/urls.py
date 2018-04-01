a9fcd3dc3d9c63b724c2dad39963212584c52457
Stanislav <swatogor@mail.ru>
2018-04-01 16:33:11 +0200

    I've changed url(r'^api/organisations/') to the url(r'^api/organisations/').

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
 
 
 urlpatterns = [
     url(r'^business/', include('business.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/organisations/', include('business.api.urls')),    
+    url(r'^api/', include('business.api.urls')),  
 ]
